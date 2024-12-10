01fd7e1e993cfe1cb49381026da1a6d8f1c8e07e
nghia46 <nghialoe46@gmail.com>
2024-12-10 14:18:43 +0700

    fix swagger route

--- src/book_controller.py
+++ src/book_controller.py
@@ -1,20 +1,24 @@
+import os
 from flask import jsonify, request
 from bson.objectid import ObjectId
-from book import Book
 from app import app, book_service
 from flasgger import swag_from
 
+# Đường dẫn tuyệt đối tới thư mục swagger
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))  # Lấy thư mục chứa file controller
+SWAGGER_DIR = os.path.join(BASE_DIR, '../swagger')  # Lùi lại 2 cấp tới thư mục gốc
+
 # Route to retrieve all books
 @app.route('/api/books', methods=['GET'])
-@swag_from('swagger/book/get_books.yml', methods=['GET'], endpoint='get_books')
+@swag_from(os.path.join(SWAGGER_DIR, 'book/get_books.yml'))
 def get_books():
     books = book_service.get_all_books()
     serialized_books = [{**book, '_id': str(book['_id'])} for book in books]
     return jsonify(serialized_books)
 
 # Route to retrieve a single book by its ID
-@app.route('/api/books/<string:book_id>', methods=['GET'])
-@swag_from('swagger/book/get_book.yml', methods=['GET'], endpoint='get_book')
+@app.route('/api/book/<string:book_id>', methods=['GET'])
+@swag_from(os.path.join(SWAGGER_DIR, 'book/get_book.yml'))
 def get_book(book_id):
     book = book_service.get_book_by_id(ObjectId(book_id))
     if book:
@@ -25,7 +29,7 @@ def get_book(book_id):
 
 # Route to create a new book
 @app.route('/api/books', methods=['POST'])
-@swag_from('swagger/book/create_book.yml', methods=['POST'], endpoint='create_book')
+@swag_from(os.path.join(SWAGGER_DIR, 'book/create_book.yml'))
 def create_book():
     data = request.json
     book_id = book_service.create_book(data)
@@ -33,7 +37,8 @@ def create_book():
 
 # Route to update book
 @app.route('/api/books/<string:book_id>', methods=['PUT'])
-@swag_from('swagger/book/update_book.yml', methods=['PUT'], endpoint='update_book')
+@swag_from(os.path.join(SWAGGER_DIR, 'book/update_book.yml'))
+
 def update_book(book_id):
     # Get data from request body
     data = request.json
@@ -50,7 +55,7 @@ def update_book(book_id):
     
 # Route to delete book
 @app.route('/api/books/<string:book_id>', methods=['DELETE'])
-@swag_from('swagger/book/delete_book.yml', methods=['DELETE'], endpoint='delete_book')
+@swag_from(os.path.join(SWAGGER_DIR, 'book/delete_book.yml'))
 def delete_book(book_id):
     success = book_service.delete_book(ObjectId(book_id))
     if success:
